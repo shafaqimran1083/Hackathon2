15c05acece5fb310bb64b7ef094aa28906f1ed50
Shafaq Naz <107723802+shafaqimran1083@users.noreply.github.com>
2023-08-27 21:10:14 +0500

    Update firebase.mjs

--- JS/firebase.mjs
+++ JS/firebase.mjs
@@ -1,17 +1,16 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.2.0/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.2.0/firebase-auth.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.2.0/firebase-firestore.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.2.0/firebase-database.js";
 import { getStorage } from "https://www.gstatic.com/firebasejs/10.2.0/firebase-storage.js";
  
 const firebaseConfig = {
-  apiKey: "AIzaSyBqeEsz-OOPdLh-6QVFixYq_eOLVey_-Bs",
-  authDomain: "shafaq-ad46c.firebaseapp.com",
-  databaseURL: "https://shafaq-ad46c-default-rtdb.firebaseio.com",
-  projectId: "shafaq-ad46c",
-  storageBucket: "shafaq-ad46c.appspot.com",
-  messagingSenderId: "786582256482",
-  appId: "1:786582256482:web:8b6126b016345f39680a3b",
-  measurementId: "G-VS4V81MM3Y"
+  apiKey: "AIzaSyBA9_3Y0jx8TgP-Iaq92ZUWY2az669tUU4",
+  authDomain: "hackathone2-92be7.firebaseapp.com",
+  projectId: "hackathone2-92be7",
+  storageBucket: "hackathone2-92be7.appspot.com",
+  messagingSenderId: "160032294130",
+  appId: "1:160032294130:web:766fa9d777b0e3fc5ebc6b",
+  measurementId: "G-HC2ZCWRKV0"
 };
 const app = initializeApp(firebaseConfig);
 const auth=getAuth(app);
